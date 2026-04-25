85b3648e4e82961d5afd83e8841dcdf321155e3e
Serhii Kulakivskyi <kulakivskyi.s.v@gmail.com>
2026-04-25 15:32:41 +0300

    Add speeds

--- components/audioPlayer/types.ts
+++ components/audioPlayer/types.ts
@@ -25,8 +25,36 @@ export const SPEEDS = [
     value: 0.5,
   },
   {
+    label: "0.7x",
+    value: 0.7,
+  },
+  {
+    label: "0.8x",
+    value: 0.8,
+  },
+  {
+    label: "0.9x",
+    value: 0.9,
+  },
+  {
     label: "1x",
     value: 1,
+  },
+  {
+    label: "1.1x",
+    value: 1.1,
+  },
+  {
+    label: "1.2x",
+    value: 1.2,
+  },
+  {
+    label: "1.3x",
+    value: 1.3,
+  },
+  {
+    label: "1.4x",
+    value: 1.4,
   },
   {
     label: "1.5x",
